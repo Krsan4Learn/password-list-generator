f34ab1537cd2282b5c60cc3066dbb01fa04ad82a
Ammar Khairy <72611745+Krsan4Learn@users.noreply.github.com>
2021-06-29 21:59:22 +0300

    password list generator
    for password list generator

---
+++ pw list generator.py
@@ -0,0 +1,22 @@
+# Code By Krsan4Learn
+# Code With Python3
+# Make Your WordList
+
+from itertools import product
+import string
+
+min_len = int(input("Enter Your Min Len --> :"))
+max_len = int(input("Enter Your Max Len --> : "))
+counter = 0
+print("[+] Loding . . . ")
+charter = string.ascii_uppercase+string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
+file_open = open("WordList.txt","w")
+
+for i in range(min_len,max_len+1):
+    for s in product(charter,repeat=i):
+        word = "".join(s)
+        file_open.write(word)
+        file_open.write("\n")
+        counter+=1
+
+print("WordList Genrate As {}".format(counter))
